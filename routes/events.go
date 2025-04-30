48a0318f90052db1dabf9647090b70bb0aefc48a
Coletinn <gustavocoleto@hotmail.com>
2025-04-30 18:10:35 -0300

    added middleware

--- routes/events.go
+++ routes/events.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"rest-api/models"
-	"rest-api/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -35,26 +34,8 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-
-	if token == "" {
-		context.JSON(
-			http.StatusUnauthorized,
-			gin.H{"message": "Authorization failed"})
-			return
-	}
-
-	err := utils.VerifyToken(token)
-
-	if err != nil {
-		context.JSON(
-			http.StatusUnauthorized,
-			gin.H{"message": "Authentication failed"})
-		return
-	}
-
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(
@@ -63,7 +44,9 @@ func createEvent(context *gin.Context) {
 			return
 	}
 
-	event.UserID = 1
+	userId := context.GetInt64("userId")
+	event.UserID = userId
+	
 	err = event.Save()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event"})
